ebbb47f32fe99f93fc81a72a75cf99b7e765c9e7
PapiMatteo <matteopapi.work@gmail.com>
2024-02-05 10:38:01 +0100

    add:api show route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
+use function PHPSTORM_META\type;
+
 class ProjectController extends Controller
 {
     public function index(){
@@ -13,5 +15,21 @@ class ProjectController extends Controller
         return response()->json([
             'results'=> $projects
         ]);
+    }
+
+    public function show(string $slug) {
+        $project = Project::with('technologies', 'type')->where('slug', $slug)->first();
+
+        if($project) {
+            return response()->json([
+                'results' => $project,
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Nessun progetto trovato'
+            ]);
+        }
     }
 }
